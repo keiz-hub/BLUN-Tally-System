ca1dab744cb3b79e517c281322d9981226d7034d
keiz-hub <genaldandres2003@gmail.com>
2025-08-18 08:36:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-
 const contestants = ["Alice", "Bella", "Carla", "Diana", "Ella"];
 const categories = [
+  { name: "Casual Attire", weight: 0.2 },
+  { name: "Sports Attire", weight: 0.2 },
   { name: "Talent", weight: 0.3 },
-  { name: "Beauty", weight: 0.4 },
-  { name: "Q&A", weight: 0.3 }
+  { name: "Blank 1", weight: 0.15 },
+  { name: "Blank 2", weight: 0.15 }
 ];
 const judges = ["Judge 1", "Judge 2", "Judge 3", "Judge 4", "Judge 5"];
